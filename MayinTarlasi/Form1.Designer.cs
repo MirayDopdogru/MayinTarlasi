77a37e949e103e3548568dd5dfec8da40601f1bd
Miray <miraydopdogru@gmail.com>
2019-05-23 14:46:49 +0300

    Add project files.

---
+++ MayinTarlasi/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace MayinTarlasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtBombs = new System.Windows.Forms.TextBox();
+            this.txtTimer = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtBombs
+            // 
+            this.txtBombs.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtBombs.Location = new System.Drawing.Point(255, 424);
+            this.txtBombs.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtBombs.Name = "txtBombs";
+            this.txtBombs.ReadOnly = true;
+            this.txtBombs.Size = new System.Drawing.Size(148, 24);
+            this.txtBombs.TabIndex = 4;
+            this.txtBombs.Text = "10";
+            this.txtBombs.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtTimer
+            // 
+            this.txtTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtTimer.Location = new System.Drawing.Point(84, 424);
+            this.txtTimer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtTimer.Name = "txtTimer";
+            this.txtTimer.ReadOnly = true;
+            this.txtTimer.Size = new System.Drawing.Size(148, 24);
+            this.txtTimer.TabIndex = 5;
+            this.txtTimer.Text = "0";
+            this.txtTimer.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(411, 427);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "MAYIN";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(11, 427);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ZAMAN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(541, 462);
+            this.Controls.Add(this.txtBombs);
+            this.Controls.Add(this.txtTimer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Mayın Tarlası";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtBombs;
+        private System.Windows.Forms.TextBox txtTimer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MayinTarlasi/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayinTarlasi
+{
+    public partial class Form1 : Form
+    {
+        Image mineImage = Image.FromFile("mines.png");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BuildControls();
+            GetBombs();
+            GetCounterByBombs();
+        }
+
+        private void GetCounterByBombs()
+        {
+            Label lbl;
+            foreach (Control item in this.Controls)
+            {
+                if (item is Label && item.Name != "label1" && item.Name != "label2")
+                {
+                    lbl = item as Label;
+                    if (lbl.Image != mineImage)
+                    {
+                        lbl.Text = GetCounter(lbl);
+                    }
+                }
+            }
+        }
+
+        private string GetCounter(Label lbl)
+        {
+            int counter = 0;
+            Label currentLabel = null;
+
+            foreach (Control item in this.Controls)
+            {
+                if (item is Label)
+                {
+                    currentLabel = item as Label;
+                    if (currentLabel.Location.X == lbl.Location.X - 35)
+                    {
+                        if (currentLabel.Location.Y == lbl.Location.Y - 35 || currentLabel.Location.Y == lbl.Location.Y || currentLabel.Location.Y == lbl.Location.Y + 35)
+                        {
+                            if (currentLabel.Image == mineImage)
+                                counter++;
+                        }
+                    }
+                    else if (currentLabel.Location.X == lbl.Location.X)
+                    {
+                        if (currentLabel.Location.Y == lbl.Location.Y - 35 || currentLabel.Location.Y == lbl.Location.Y + 35)
+                        {
+                            if (currentLabel.Image == mineImage)
+                                counter++;
+                        }
+                    }
+                    else if (currentLabel.Location.X == lbl.Location.X + 35)
+                    {
+                        if (currentLabel.Location.Y == lbl.Location.Y - 35 || currentLabel.Location.Y == lbl.Location.Y || currentLabel.Location.Y == lbl.Location.Y + 35)
+                        {
+                            if (currentLabel.Image == mineImage)
+                                counter++;
+                        }
+                    }
+                }
+            }
+
+            return counter.ToString();
+        }
+
+        private void GetBombs()
+        {
+            Random rnd = new Random();
+            int generatedX = 0,
+                generatedY = 0;
+            string labelName = string.Empty;
+            Label currentLabel;
+
+            for (int i = 0; i < 10; i++)
+            {
+                generatedX = rnd.Next(10);
+                generatedY = rnd.Next(10);
+                labelName = "lbl" + generatedX.ToString() + generatedY.ToString();
+
+                currentLabel = this.Controls.Find(labelName, false)[0] as Label;
+                if (currentLabel.Image != mineImage)
+                {
+                    currentLabel.Image = mineImage;
+                }
+                else
+                {
+                    i--;
+                }
+            }
+        }
+
+        private void BuildControls()
+        {
+            Button btn;
+            Label lbl;
+
+            int x = 50,
+                y = 50;
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    lbl = new Label();
+                    lbl.Name = "lbl" + i.ToString() + j.ToString();
+                    lbl.Text = "";
+                    lbl.TextAlign = ContentAlignment.MiddleCenter;
+                    lbl.Size = new Size(35, 35);
+                    lbl.Location = new Point(x, y);
+                    lbl.Hide(); // Visible = false;
+                    this.Controls.Add(lbl);
+
+                    btn = new Button();
+                    btn.Name = "btn" + i.ToString() + j.ToString();
+                    btn.Size = new Size(35, 35);
+                    btn.Location = new Point(x, y);
+                    btn.BackColor = Color.LightSeaGreen;
+                    btn.FlatStyle = FlatStyle.Flat;
+                    btn.FlatAppearance.MouseDownBackColor = Color.PaleTurquoise;
+                    btn.MouseDown += Btn_MouseDown;
+                    btn.Tag = lbl;
+                    this.Controls.Add(btn);
+
+                    x += 35;
+                }
+
+                y += 35;
+                x = 50;
+            }
+        }
+
+        private void Btn_MouseDown(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+            if (e.Button == MouseButtons.Left)
+            {
+                btn.Visible = false;
+                Label lbl = btn.Tag as Label;
+                lbl.Show();
+                if (lbl.Image == mineImage)
+                {
+                    //timer1.Enabled = false;
+                    timer1.Stop();
+                    MessageBox.Show("Game Over");
+                    Application.Exit();
+                }
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                btn.BackgroundImage = Image.FromFile("flag.png");
+                btn.BackgroundImageLayout = ImageLayout.Stretch;
+                int mines = int.Parse(txtBombs.Text);
+                txtBombs.Text = (mines - 1).ToString();
+            }
+
+            //timer1.Enabled = true;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int second = int.Parse(txtTimer.Text);
+            second++;
+            txtTimer.Text = second.ToString();
+        }
+    }
+}
